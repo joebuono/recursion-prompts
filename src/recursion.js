9e47b2cb7a8a4dc9e8ff230b6df2a6743e71279a
Joe Buono <j.buono7@yahoo.com>
2020-08-25 23:42:27 -0400

    First commit, recursion exercises

--- src/recursion.js
+++ src/recursion.js
@@ -7,16 +7,31 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+  if (n < 0) return null;
+  if (n < 1) return 1;
+  return n * factorial(n-1);
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+  if (!array.length) return 0;
+  let num = array[0];
+  return num + sum(array.slice(1));
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  let sum = 0;
+  for (let i of array) {
+    if (Array.isArray(i)) {
+      sum += arraySum(i);
+    } else {
+      sum += i;
+    }
+  }
+  return sum;
 };
 
 // 4. Check if a number is even.
